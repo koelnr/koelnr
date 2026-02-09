b2d6f3bed3088ffee38536cc4087e5b10e2eace9
Harsh Sandhu <hsandhu@speareducation.com>
2026-02-09 20:20:05 +0530

    refactor: move createPlaceholderImg util function

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const PLACEHOLDER_BASE = "https://placehold.harshsandhu.com/api/img";
+
+export function createPlaceholderImg(
+  w: number,
+  h: number,
+  bg = "0a0a0a",
+  fg = "fafafa",
+) {
+  return `${PLACEHOLDER_BASE}?w=${w}&h=${h}&bg=${bg}&fg=${fg}`;
 }
