1765d77842d28d193e923498776793cc9951e753
Harsh Sandhu <hsandhu@speareducation.com>
2026-02-09 22:35:26 +0530

    feat: setup sign-in and sign-up routes

--- apps/web/src/routes/router.ts
+++ apps/web/src/routes/router.ts
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from ".";
 import { Home } from "./home";
+import { SignIn } from "./auth/sign-in";
+import { SignUp } from "./auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -8,4 +10,6 @@ export const router = createBrowserRouter([
     Component: RootLayout,
     children: [{ index: true, Component: Home }],
   },
+  { path: "/sign-in", Component: SignIn },
+  { path: "/sign-up", Component: SignUp },
 ]);
